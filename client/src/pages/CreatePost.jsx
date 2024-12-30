224716c8b8bd92af5c86a831d8060eb7cfe63041
dimainc26 <dimainc26@gmail.com>
2024-12-30 23:43:55 +0100

    update BE url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,7 +15,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
 
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://di-mage.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -44,7 +44,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post/", {
+        const response = await fetch("https://di-mage.onrender.com/api/v1/post/", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
     app.listen(8080, () =>
-      console.log("Server has started on port http://localhost:8080")
+      console.log("Server has started on port 8080")
     );
   } catch (err) {
     console.log(err)
